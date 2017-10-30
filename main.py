00eca1c2101e7d6bb73335ddbbc322ffe1ba5a3c
wanghao <1179216825@qq.com>
2017-10-30 23:33:47 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# 扣件模型训练检测
+
+import sys
+from PyQt4 import QtGui
+from gui.FTain import MainWindow
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    ftt = MainWindow()
+    sys.exit(app.exec_())
